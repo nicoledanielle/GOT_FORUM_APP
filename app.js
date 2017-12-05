688fc08576d666352d3654c8c42fdbbc6e4b820f
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-12-05 07:48:14 -0800

    added some dummy data

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let MOCK_FORUM_POSTS = {
+  'generalPosts': [
+    {
+      'id': '111111',
+      'title': 'What character do you identify most with and why?',
+      'author': 'whitewalker',
+      'content': 'It\'s ten thousand miles between Kings landing and the wall. The winds of Winter. Dunc the Lunk, thick as a castle wall. The bear and the maiden fair. A Lannister always pays his debts. The battle of the redgrass field. Hear me roar! Our Sun Shines Bright. More pigeon pie, please. It\'s ten thousand miles between Kings landing and the wall.',
+      'publishedAt': '11-11-2017',
+      'comments': {
+        'number': 1,
+        'comments': [
+          {'id': '234567',
+            'author': 'mskhaleesi',
+            'content': 'Definitely Danaerys Targaryen as you can tell by my username',
+            'publishedAt': '11-11-2017'}
+        ]
+      }
+    },
+    {
+      'id': '111112',
+      'title': 'What character do you identify most with and why?',
+      'author': 'mskhaleesi',
+      'content': 'Ezas eshna gech ahilee! Fini hazi? Hash yer dothrae chek asshekh? Ifas maisi yeri. Ki fin yeni? Yer affesi anni. Nevakhi vekha ha maan: Rekke, m\'aresakea norethi fitte. Yer zheanae sekke. Jalan atthirari anni. Shekh ma shieraki anni.',
+      'publishedAt': '9-20-2017',
+      'comments': {
+        'number': 0,
+        'comments': []
+      }
+    },
+  ]
+};
