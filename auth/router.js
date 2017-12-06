79e0272198859db27f581f1ca142eb1086e3b7cd
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-12-05 16:02:11 -0800

    added files for auth & note on the ajax request

--- auth/router.js
+++ auth/router.js
@@ -1,1 +1,72 @@
 'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const config = require('../config');
+const router = express.Router();
+
+const {User} = require('../models');
+
+const createAuthToken = function(user) {
+  return jwt.sign({user}, config.JWT_SECRET, {
+    subject: user.username,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+const localAuth = passport.authenticate('local', {session: false});
+router.use(bodyParser.json());
+// The user provides a username and password to login
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user.apiRepr());
+  res.json({authToken});
+});
+
+router.post('/register', (req, res) => {
+  
+  let {username, password, firstName = '', lastName = ''} = req.body;
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+  
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr());
+    })
+    .catch(err => {
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
+});
+  
+
+module.exports = {router};
