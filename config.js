0916500c0218287e4861d04b59fb39216f96beeb
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-12-08 13:02:46 -0800

    final test

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        'mongodb://localhost/got-forum-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                        'mongodb://localhost/test-got-forum-app';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 8080; 
